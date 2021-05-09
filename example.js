b8f3c0604eec4418504e2b4828310cbbdc60846e
LuanRT <luan.lrt4@gmail.com>
2021-05-09 03:30:08 -0300

    Delete example.js

--- example.js
+++
@@ -1,8 +0,0 @@
-const googleThis = require('./googleThis');
-
-googleThis('Minecraft')
-   .then((results) => {
-       console.log(results);
-   }).catch ((err) => {
-       console.log('Hm, something went wrong!\n'+err);
-   });
