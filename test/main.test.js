5b923fdce3c4ba36547493955fd398c7b3329394
Wazbat <wazbat97@gmail.com>
2023-08-21 21:46:22 +0200

    ✅ Added tests and removed non-working type check

--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const google = require('..');
+const axios = require('axios');
 
 describe('GoogleThis Tests', () => {
   it('Should search a query', async () => {
@@ -21,5 +22,15 @@ describe('GoogleThis Tests', () => {
   it('Should retrieve top news', async () => {
     const news = await google.getTopNews('en', 'AU');
     expect(news.headline_stories).not.toHaveLength(0);
+  });
+
+  describe('custom axios instances', () => {
+    it('should support a custom axios instance', async () => {
+      const instance = axios.create();
+      const getSpy = jest.spyOn(instance, 'get');
+      const search = await google.search('Stephen Hawking', { axios_instance: instance });
+      expect(search.results).not.toHaveLength(0);
+      expect(getSpy).toHaveBeenCalledTimes(1);
+    });
   });
 });
